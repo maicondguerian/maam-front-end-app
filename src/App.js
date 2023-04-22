441b5861e82f87f0368b6a838ab33283d26a2b9b
maicondguerian <maicondguerian@gmail.com>
2023-04-21 23:58:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { LeftSidebarProvider } from './Context/LeftSidebarContext';
+import LeftSidebar from './components/LeftSidebar/LeftSidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LeftSidebarProvider>
+      <LeftSidebar />
+
+    </LeftSidebarProvider>
   );
 }
 
---
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftSidebar = () => {
+  return (
+    <div>
+        asdsad
+    </div>
+  )
+}
+
+export default LeftSidebar
